4edf00616b82fa99de26fad89785ed4e523d2a42
needle58 <needlepnz@gmail.com>
2023-08-16 15:13:31 +0300

    fist try

--- 2.2.3.py
+++ 2.2.3.py
@@ -9,6 +9,8 @@ try:
     browser.get('http://suninjuly.github.io/selects1.html')
     num1 = int(browser.find_element(By.ID, 'num1').text)
     num2 = int(browser.find_element(By.ID, 'num2').text)
-
+    ans = str(num1 + num2)
+    fall_list = Select(browser.find_element(By.TAG_NAME, 'select'))
+    fall_list.select_by_value(num1 + num2)
 finally:
     browser.quit()
