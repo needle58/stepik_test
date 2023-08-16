f7f9b13d0cdad6dc801dfe1c4281b412ff9932a5
needle58 <needlepnz@gmail.com>
2023-08-16 15:51:50 +0300

    fist try

---
+++ 2.2.4.py
@@ -0,0 +1,9 @@
+import time
+
+from selenium import webdriver
+
+browser = webdriver.Chrome()
+# browser.execute_script("alert('Robots at work');")
+# browser.execute_script("document.title='Script executing';")
+browser.execute_script("document.title='Script executing';alert('Robots at work');")
+time.sleep(5)
