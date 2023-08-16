bb762df0c2c63ae34d102be934266e6696087a81
needle58 <needlepnz@gmail.com>
2023-08-16 15:20:28 +0300

    fist try

--- 2.2.3.py
+++ 2.2.3.py
@@ -11,6 +11,8 @@ try:
     num2 = int(browser.find_element(By.ID, 'num2').text)
     ans = str(num1 + num2)
     fall_list = Select(browser.find_element(By.TAG_NAME, 'select'))
-    fall_list.select_by_value(num1 + num2)
+    fall_list.select_by_value(ans)
+    browser.find_element(By.CLASS_NAME, 'btn').click()
 finally:
+    time.sleep(3)
     browser.quit()
