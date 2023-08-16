39eb0c6aaa4876d2d7da5c5666b87788e025590d
needle58 <needlepnz@gmail.com>
2023-08-16 15:07:52 +0300

    fist try

---
+++ 2.2.3.py
@@ -0,0 +1,14 @@
+import time
+import math
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+
+try:
+    browser = webdriver.Chrome()
+    browser.get('http://suninjuly.github.io/selects1.html')
+    num1 = int(browser.find_element(By.ID, 'num1').text)
+    num2 = int(browser.find_element(By.ID, 'num2').text)
+
+finally:
+    browser.quit()
